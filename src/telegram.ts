96f83495a212c98e850fb0139bf978ba820ea921
Zoe <blink_zoe@hotmail.com>
2026-06-30 18:11:52 -0600

    Finish

--- src/telegram.ts
+++ src/telegram.ts
@@ -1,7 +1,7 @@
 // src/services/telegram.ts
 
 import axios from "axios";
-import { config } from "../config/config.js";
+import { config } from "./config/config.js";
 
 export async function sendTelegram(message: string): Promise<void> {
     const url = `https://api.telegram.org/bot${config.telegramToken}/sendMessage`;
